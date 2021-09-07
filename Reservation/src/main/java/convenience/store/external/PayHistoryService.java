6c5b9568cfd39f16f1f339d6b8f07e23f44f6768
djjoung <nkjdj@naver.com>
2021-09-07 13:12:14 +0900

    commit Reservation for cloud

--- Reservation/src/main/java/convenience/store/external/PayHistoryService.java
+++ Reservation/src/main/java/convenience/store/external/PayHistoryService.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.Date;
 
-@FeignClient(name="Pay", url="http://localhost:8082/", fallback = PayHistoryServiceImpl.class)  // for local
-//@FeignClient(name="Pay", url="http://Pay:8080")  // for cloud
+//@FeignClient(name="Pay", url="http://localhost:8082/", fallback = PayHistoryServiceImpl.class)  // for local
+@FeignClient(name="Pay", url="http://Pay:8080")  // for cloud
 public interface PayHistoryService {
     @RequestMapping(method= RequestMethod.POST, path="/payHistories/request")
     public boolean request(@RequestBody PayHistory payHistory);
