7d9e68638143dcb454ac90c10bbfaf1571884ce7
alex joung <nkjdj@naver.com>
2021-09-07 20:43:56 +0900

    Update PayHistory.java

--- Pay/src/main/java/convenience/store/PayHistory.java
+++ Pay/src/main/java/convenience/store/PayHistory.java
@@ -30,11 +30,10 @@ public class PayHistory {
 		System.out.println("\n\n##### PayHistory onPrePersist  " + /* payRequested.toJson() + */"\n\n");
 
         // HPA test 용 지연 코드.
-        try {
-            Thread.currentThread().sleep((long) (2400 + Math.random() * 220));
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }             
+        double dLoad = 0.0001;
+        for (int iCnt = 0; iCnt <= 1000000; iCnt++) {
+            dLoad += Math.sqrt(dLoad);
+        }           
 	}
 
 
